b0b75e9f621391d20e6d2b01eb66a83a748b2c78
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-09 11:06:55 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function hexToRgb(hex){
+	var result = /^#?([a-f\d]{2}])([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  	
+ 	return result ? {
+    	r: parseInt(result[1],  16);
+      	g: parseInt(result[2],  16);
+  		b: parseInt(result[3],  16);
+    } : null;
+}
+var hex = "#0a3678";
+console.log(hexToRgb(hex).r+","+hexToRgb(hex).g+","+hexToRgb(hex).b);//10,54,120
